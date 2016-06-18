5be116013209347b9be382820fa3695ab596f11b
dainixiao <2819666156@qq.com>
2016-06-18 19:08:29 +0800

    提交代码

---
+++ dqscrapy/pipelines.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
+
+from dqscrapy.impl import QiniuFilesStore
+
+
+class DqscrapyPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+class DqScrapyPipeline(object):
+    def open_spider(self, spider):
+        # self.dbconn = connect('douqu', host='192.168.1.45:27017')
+        # self.connction = pymongo.Connection("192.168.1.45", 27017)
+        # self.douqu = self.connction.douqu
+        self.mongoClient = MongoClient("192.168.1.45", 27017)
+        self.db = self.mongoClient.douqu
+
+    def close_spider(self, spider):
+        pass
+    def process_item(self, item, spider):
+        print 'wowowoowowoow'
+        # downLoadUrl = item['avatarUrl']
+        # self.db.qsbk.insert(dict(item))
+        # self.dbconn.in
+        return item
---
+++ dqscrapy/spiders/dqSpider.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Liuwf on 16/5/8
+from scrapy.spiders import CrawlSpider, Rule
+
+from dqscrapy.items import QsbkItem
+from scrapy.linkextractors import LinkExtractor
+
+class DmozSpider(CrawlSpider):
+    name = "qsbkcrawl"
+    start_urls = [
+        "http://www.qiushibaike.com"
+    ]
+    rules = [
+        Rule(LinkExtractor(allow=('/8hr/page/[1-3]*')),callback='parse_list',follow=True)
+    ]
+
+
+    def parse_list(self, response):
+        from scrapy import Selector
+        sel = Selector(response)
+        sites = sel.xpath('//*[@id="content-left"]/div[@class="article block untagged mb15"]')
+        items = []
+        for site in sites:
+            item = QsbkItem()
+            name = site.xpath('div[@class="content"]/text()').extract()
+            nick = site.xpath('div[@class="author clearfix"]/a[2]/h2/text()').extract()
+            avatarUrl = site.xpath('div[@class="author clearfix"]/a[1]/img/@src').extract()
+            item['desc'] = name
+            item['nick'] = nick
+            item['avatarUrl'] = avatarUrl
+            items.append(item)
+        return items
