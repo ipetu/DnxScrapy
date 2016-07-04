9e1f824472806b61ef8631481a271e271973a38e
dainixiao <2819666156@qq.com>
2016-07-05 00:03:11 +0800

    增加测试文件

--- dqscrapy/youtubeDowload.py
+++ dqscrapy/youtubeDowload.py
@@ -1,23 +1,58 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by Liuwf on 16/6/28
+from __future__ import unicode_literals
 import youtube_dl
 
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
+class MyLogger(object):
+    def debug(self, msg):
+        print 'msgDebug-->' + msg
+
+    def warning(self, msg):
+        print 'msgWarning-->' + msg
+
+    def error(self, msg):
+        print 'msgError--->' + msg
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'mp4',
+    'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4'
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+    'outtmpl': '%(id)s.%(ext)s',
+    'writeinfojson': True,
+    'ignoreerrors': True,
+    'simulate': True,
+}
+
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+playList = ['https://www.youtube.com/watch?v=cmnX7iYK1Nk', 'https://www.youtube.com/watch?v=6Lwkkem7hMw',
+            'https://www.youtube.com/watch?v=b_qsx9uNhi0']
 with ydl:
-    result = ydl.extract_info(
-        'http://v.youku.com/v_show/id_XMTYyMTk4OTU0OA==.html?f=26378220&from=y1.3-idx-beta-1519-23042.223465.4-3',
-        download=False # We just want to extract the info
-    )
-
-if 'entries' in result:
-    # Can be a playlist or a list of videos
-    video = result['entries'][0]
-else:
-    # Just a video
-    video = result
-
-print(video)
-video_url = video['url']
-print(video_url)
+    # result = ydl.extract_info(
+    #     ['https://www.youtube.com/watch?v=cmnX7iYK1Nk', 'https://www.youtube.com/watch?v=6Lwkkem7hMw',
+    #      'https://www.youtube.com/watch?v=b_qsx9uNhi0'],
+    #     download=False  # We just want to extract the info
+    # )
+    # result = ydl.download(playList)
+    ydl.list_formats()
+# if 'entries' in result:
+#     # Can be a playlist or a list of videos
+#     video = result['entries'][0]
+# else:
+#     # Just a video
+#     video = result
+#
+# print(video)
+# video_url = video['url']
+# print(video_url)
